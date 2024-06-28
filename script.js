ffb137e6d2db507da68d366d7c5db47dd0ff9c4c
hassan1222 <Hassanatta2013@gmail.com>
2024-06-29 01:18:22 +0500

    My Portfolio
    .

---
+++ script.js
@@ -0,0 +1,49 @@
+function openMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('open');
+}
+
+function downloadCV() {
+  window.location.href = 'assets/Hassan_Shahzad_CV.pdf';
+}
+
+let slideIndex1 = 0;
+let slideIndex2 = 0;
+let slideIndex3 = 0;
+
+function showSlides1() {
+  let slides = document.getElementsByClassName("mySlides1");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex1++;
+  if (slideIndex1 > slides.length) {slideIndex1 = 1}    
+  slides[slideIndex1-1].style.display = "block";  
+  setTimeout(showSlides1, 2000); // Change image every 2 seconds
+}
+
+function showSlides2() {
+  let slides = document.getElementsByClassName("mySlides2");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex2++;
+  if (slideIndex2 > slides.length) {slideIndex2 = 1}    
+  slides[slideIndex2-1].style.display = "block";  
+  setTimeout(showSlides2, 2000); // Change image every 2 seconds
+}
+
+function showSlides3() {
+  let slides = document.getElementsByClassName("mySlides3");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex3++;
+  if (slideIndex3 > slides.length) {slideIndex3 = 1}    
+  slides[slideIndex3-1].style.display = "block";  
+  setTimeout(showSlides3, 2000); // Change image every 2 seconds
+}
+
+showSlides1();
+showSlides2();
+showSlides3();
